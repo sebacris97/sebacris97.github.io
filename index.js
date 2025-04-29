ffaedc95c6ccb8fa760b7b79a459ac116294347b
sebacris97 <counterlaplata@live.com>
2025-04-29 03:10:27 -0300

    fixed cart counter not updating with localstorage data

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 
-// variable global para almacenar los productos seleccionados
-let carrito = [];
+// funcion para actualizar el contador del carrito
+const actualizarContador = ()=>{
+    //cambiamos el contenido del HTML con el ID contador-carrito
+    document.getElementById("contador-carrito").textContent = carrito.length
+
+}
+
+// Recuperar el carrito del almacenamiento local
+const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+actualizarContador()
 
 const agregarAlcarrito = (nombre,precio) =>{
     //agregar el producto como un objeto al carrito
     carrito.push({nombre,precio})
-
+    localStorage.setItem("carrito",JSON.stringify(carrito))
     // actualizar el contador visual del carrito
     actualizarContador()
     // muestra un alerta de confirmacion
@@ -13,12 +21,6 @@ const agregarAlcarrito = (nombre,precio) =>{
 }
 
 
-// funcion para actualizar el contador del carrito
-const actualizarContador = ()=>{
-    //cambiamos el contenido del HTML con el ID contador-carrito
-    document.getElementById("contador-carrito").textContent = carrito.length
-
-}
 
 // Guarda el contenido del carrito en el almacenamiento local antes de cerrar la pagina
 
